dc59f74f31faa7b70bde993ea32091eb0fb90485
Paul Joey Clark <joey@neuralyte.org>
2014-11-17 03:39:06 +0800

    Show how many commits this branch is ahead/behind the remote

--- prompt.sh
+++ prompt.sh
@@ -20,13 +20,33 @@ find_git_dirty() {
   fi
 }
 
-PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+find_git_ahead_behind() {
+  local local_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+  if [[ -n "$local_branch" ]]; then
+    local upstream_branch=$(git rev-parse --abbrev-ref "@{upstream}" 2> /dev/null)
+    # If the branch is not tracking a specific remote branch, then assume we are tracking origin/[this_branch_name]
+    [[ -z "$upstream_branch" ]] && upstream_branch="origin/$local_branch"
+    if [[ -n "$upstream_branch" ]]; then
+      local  ahead=$(git rev-list --left-right ${local_branch}...${upstream_branch} 2> /dev/null | grep -c '^<')
+      local behind=$(git rev-list --left-right ${local_branch}...${upstream_branch} 2> /dev/null | grep -c '^>')
+      if [[ "$ahead" = 0 ]] && [[ "$behind" = 0 ]]; then
+        git_ahead_behind=''
+      else
+        git_ahead_behind=""
+        [[ "$ahead"  != 0 ]] && git_ahead_behind="$git_ahead_behind+$ahead"
+        [[ "$behind" != 0 ]] && git_ahead_behind="$git_ahead_behind-$behind"
+      fi
+    fi
+  fi
+}
+
+PROMPT_COMMAND="find_git_branch; find_git_dirty; find_git_ahead_behind; $PROMPT_COMMAND"
 
 # Default Git enabled prompt with dirty state
-# export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+# export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\$git_ahead_behind\[$txtrst\]\$ "
 
 # Another variant:
-# export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\[$txtrst\]\$ "
+# export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\$git_ahead_behind\[$txtrst\]\$ "
 
 # Default Git enabled root prompt (for use with "sudo -s")
 # export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
