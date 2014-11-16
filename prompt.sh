fa154c1ccfd14f96867e1991026531700d43a0ff
Paul Joey Clark <joey@neuralyte.org>
2014-11-17 03:46:03 +0800

    Do not mark dirty solely due to untracked files

--- prompt.sh
+++ prompt.sh
@@ -12,7 +12,7 @@ find_git_branch() {
 }
 
 find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
+  local status=$(git status --porcelain 2> /dev/null | grep -v '^??')
   if [[ "$status" != "" ]]; then
     git_dirty='*'
   else
