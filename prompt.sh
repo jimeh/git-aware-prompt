f3687c9a924549d0f0152174e71d66d275553f50
Simon Vogt <simonsunimail@gmail.com>
2017-05-04 16:38:55 +0200

    Added function find_git_untracked() to show that untracked files are present. (Without using "git status") This nicely splits up the needed functionality of "git status" into two functions without the speed loss. There is now also a small function that combines both indicators into what used to be given by "git status", but without the performance loss on large repos.
    These strings can now be used in your git-aware prompt:
    
    \$git_changed - Shows if tracked files have been changed.
    \$git_untracked - Shows if untracked files (or folders) are present.
    \$git_untracked_num - Shows the number of untracked files or folders if present.
    
    \$git_dirty - Shows if tracked files have been changed or untracked files are present. (Combines the above)
    \$git_branch - The current branch, as before.

--- prompt.sh
+++ prompt.sh
@@ -11,7 +11,7 @@ find_git_branch() {
   fi
 }
 
-find_git_dirty() {
+find_git_changed() {
   local branch
   if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
     #local status=$(git status --porcelain 2> /dev/null)  # very slow for large repos!
@@ -19,26 +19,47 @@ find_git_dirty() {
     #local has_stageable_files=$(git diff-files --quiet  2> /dev/null)
     local status=$(git diff-index --quiet HEAD  2> /dev/null ; echo $?)
     if [[ "$status" == "0" ]]; then
-      git_dirty=''
+      git_changed=''
     elif [[ "$status" == "1" ]]; then
-      git_dirty='*'
+      git_changed='c'
     elif [[ "$status" == "128" ]]; then
-      git_dirty=' no_worktree'
+      git_changed=' no_worktree'
     else
-      git_dirty=" err_$status" # to show if anything else went wrong.
+      git_changed=" err_$status" # to show if anything else went wrong.
     fi
   else
-    git_dirty=''
+    git_changed=''
   fi
 }
 
-PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+find_git_untracked() {
+  git_untracked_num=$( echo $(git ls-files --other --directory --exclude-standard  2> /dev/null | wc -l) )
+  if (( "$git_untracked_num" > 0 )); then
+    git_untracked='u'
+  else
+    git_untracked=''
+    git_untracked_num=''
+  fi
+}
+
+find_git_dirty() {
+  if [[ "$git_changed" == "" ]] && [[ "$git_untracked" == "" ]]; then
+    git_dirty=''
+  else
+    git_dirty='*'
+  fi
+}
+
+PROMPT_COMMAND="find_git_branch; find_git_changed; find_git_untracked; find_git_dirty; $PROMPT_COMMAND"
 
 # Default Git enabled prompt with dirty state
 # export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
 
 # Another variant:
 # export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\[$txtrst\]\$ "
+
+# A third variant:
+# export PS1="${debian_chroot:+($debian_chroot)}\[\033[00;30m\][\D{%F %H:%M}] \[\033[00;32m\]\nyou are here: \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_changed\$git_untracked_bool\$git_dirty\[$txtrst\]\n\[\033[01;32m\]\u\[\033[00;37m\]@\[\033[00;35m\]\h\[\033[00m\]\[\033[00m\]\$ "
 
 # Default Git enabled root prompt (for use with "sudo -s")
 # export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
