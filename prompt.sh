fc093f1fd9d89474ec49021b9fd944fd6239ce1f
btara.truhandarien <btara.truhandarien@rakuten.com>
2017-06-04 11:46:09 +0900

    Short circuit based on git repo checking

--- prompt.sh
+++ prompt.sh
@@ -1,27 +1,35 @@
+is_this_a_git_repo() {
+  is_in_git=$(git rev-parse --is-inside-work-tree 2> /dev/null)
+}
+
 find_git_branch() {
   # Based on: http://stackoverflow.com/a/13003854/170413
   local branch
-  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-    if [[ "$branch" == "HEAD" ]]; then
-      branch='detached*'
+  echo $is_in_git
+  if [[ "$is_in_git" = "true" ]]; then
+    if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
+      if [[ "$branch" == "HEAD" ]]; then
+        branch='detached*'
+      fi
+      git_branch="($branch)"
     fi
-    git_branch="($branch)"
   else
     git_branch=""
   fi
 }
 
 find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
-  if [[ "$status" != "" ]]; then
-    git_dirty='*'
+  if [[ "$is_in_git" = "true" ]]; then
+    local status=$(git status --porcelain 2> /dev/null)
+    if [[ "$status" != "" ]]; then
+      git_dirty='*'
+    fi
   else
     git_dirty=''
   fi
 }
 
 find_head_tag() {
-  local is_in_git=$(git rev-parse --is-inside-work-tree 2> /dev/null)
   if [[ "$is_in_git" = "true" ]]; then
     local tag=$(git tag --column --points-at HEAD 2> /dev/null)
     if [[ ! -z $tag ]]; then
@@ -34,7 +42,7 @@ find_head_tag() {
   fi 
 }
 
-PROMPT_COMMAND="find_git_branch; find_git_dirty; find_head_tag; $PROMPT_COMMAND"
+PROMPT_COMMAND="is_this_a_git_repo; find_git_branch; find_git_dirty; find_head_tag;$PROMPT_COMMAND"
 
 # Default Git enabled prompt with dirty state
 # export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
